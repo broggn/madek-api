52046df659054e9dcfdd8a5671a055ba8ce120db
Alex Liebrich <alexanderliebrich@web.de>
2023-03-13 12:07:44 +0100

    collections create

--- src/madek/api/resources/collections.clj
+++ src/madek/api/resources/collections.clj
@@ -8,6 +8,8 @@
     [madek.api.resources.shared :as sd]
     [reitit.coercion.schema]
     [schema.core :as s]
+    [clojure.java.jdbc :as jdbc]
+    [madek.api.utils.rdbms :as rdbms]
     ))
 
 (def routes
@@ -27,6 +29,47 @@
     )
   )
 
+(def schema_collection-import 
+  {(s/optional-key :id) s/Uuid
+   (s/optional-key :get_metadata_and_previews) s/Bool
+   (s/optional-key :layout) s/Str ; TODO grid or ?
+   (s/optional-key :responsible_user_id) s/Uuid
+   (s/optional-key :clipboard_user_id) s/Any ; s/Uuid | nil
+   (s/optional-key :workflow_id) s/Any ; s/Uuid | nil
+   (s/optional-key :is_master) s/Bool
+   (s/optional-key :sorting) s/Str
+   (s/optional-key :responsible_delegation_id) s/Any ; s/Uuid | nil
+   (s/optional-key :default_context_id) s/Any ; s/Uuid | nil
+   })
+
+(def schema_collection-export
+  {
+   :id s/Uuid
+   :get_metadata_and_previews s/Bool
+   :created_at s/Any
+   :updated_at s/Any
+   :meta_data_updated_at s/Any
+   :layout s/Str ; TODO grid or ?
+   :responsible_user_id s/Uuid
+   :creator_id s/Uuid
+   :edit_session_updated_at s/Any
+   :clipboard_user_id s/Any ; s/Uuid | nil
+   :workflow_id s/Any ; s/Uuid | nil
+   :is_master s/Bool
+   :sorting s/Str
+   :responsible_delegation_id s/Any ; s/Uuid | nil
+   :default_context_id s/Any ; s/Uuid | nil
+  })
+
+(defn handle_create-collection [req]
+  (let [auth (-> req :authenticated-entity)
+        auth-id (-> auth :id)
+        req-data (-> req :parameters :body)
+        ins-data (assoc req-data :creator_id auth-id :responsible_user_id auth-id)] 
+    (if-let [ins-result (jdbc/insert! (rdbms/get-ds) "collections" ins-data)]
+      (sd/response_ok (first ins-result))
+      (sd/response_failed "Could not create collection" 406))))
+
 (def ring-routes
   ["/collections"
    ["/" {:get {:handler handle_get-index
@@ -42,8 +85,13 @@
                :coercion reitit.coercion.schema/coercion
                :responses {200 {:body {:collections [{:id s/Uuid :created_at s/Inst}]}}}}
          ; TODO
-         :post {:summary (sd/sum_todo "Create ccollection")
-                :handler (constantly sd/no_impl)}}]
+         :post {:summary (sd/sum_todo "Create collection")
+                :handler handle_create-collection
+                :swagger {:produces "application/json"}
+                :parameters {:body schema_collection-import}
+                :coercion reitit.coercion.schema/coercion
+                :responses {200 {:body schema_collection-export }}
+                }}]
 
    ["/:collection_id" {:get {:handler handle_get-collection
                              :middleware [sd/ring-wrap-add-media-resource
