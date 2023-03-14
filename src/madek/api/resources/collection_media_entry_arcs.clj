d8719fb97e90e678cd9360f662b7d181a7db9f2d
Alex Liebrich <alexanderliebrich@web.de>
2023-03-14 03:18:36 +0100

    collection-me-arcs create

--- src/madek/api/resources/collection_media_entry_arcs.clj
+++ src/madek/api/resources/collection_media_entry_arcs.clj
@@ -19,10 +19,11 @@
       sql/format))
 
 (defn arc [request]
-  (when-let [arc (->> (arc-query request)
-                      (jdbc/query (rdbms/get-ds))
-                      first)]
-    {:body arc}))
+  (let [query (arc-query request)
+        db-result (jdbc/query (rdbms/get-ds) query)]
+    (if-let [arc (first db-result)]
+      (sd/response_ok arc)
+      (sd/response_failed "No such collection-media-entry-arc" 404))))
 
 (defn arcs-query [query-params]
   (let [collection-id (-> query-params :collection_id presence)
@@ -37,9 +38,9 @@
         sql/format)))
 
 (defn arcs [request]
-  {:body {:collection-media-entry-arcs
-          (jdbc/query (rdbms/get-ds)
-                      (arcs-query (:query-params request)))}})
+  (let [query (arcs-query (-> request :parameters :query))
+        db-result (jdbc/query (rdbms/get-ds) query)]
+    (sd/response_ok {:collection-media-entry-arcs db-result})))
 
 (def routes
   (cpj/routes
@@ -47,24 +48,116 @@
     (cpj/GET "/collection-media-entry-arcs/" [] #'arcs)
     ))
 
+(defn create-col-me-arc 
+  ([col-id me-id data]
+    (create-col-me-arc col-id me-id data (rdbms/get-ds)))
+
+  ([col-id me-id data tx]
+    (let [ins-data (assoc data :collection_id col-id :media_entry_id me-id)
+          ins-res (first (jdbc/insert! tx "collection_media_entry_arcs" ins-data))]
+      ins-res))
+  
+  )
+
+(defn handle_create-col-me-arc [req]
+  (let [col-id (-> req :parameters :path :collection_id)
+        me-id (-> req :parameters :path :media_entry_id)
+        data (-> req :parameters :body)]
+    (if-let [ins-res (create-col-me-arc col-id me-id data )]
+      (sd/response_ok ins-res)
+      (sd/response_failed "Could not create collection-media-entry-arc" 406))
+    ))
+
+(def schema_collection-media-entry-arc-export
+  {
+   :id s/Uuid
+   :collection_id s/Uuid
+   :media_entry_id s/Uuid
+   :highlight s/Bool
+   :cover s/Bool
+   :order s/Num
+   :created_at s/Any
+   :updated_at s/Any
+   :position s/Int
+  })
+
+(def schema_collection-media-entry-arc-update
+  {
+   (s/optional-key :id) s/Uuid
+   (s/optional-key :collection_id) s/Uuid
+   (s/optional-key :media_entry_id) s/Uuid
+   (s/optional-key :highlight) s/Bool
+   (s/optional-key :cover) s/Bool
+   (s/optional-key :order) s/Num
+   (s/optional-key :created_at) s/Any
+   (s/optional-key :updated_at) s/Any
+   (s/optional-key :position) s/Int
+   })
+
+(def schema_collection-media-entry-arc-create
+  {
+   ;(s/optional-key :id) s/Uuid
+   ;(s/optional-key :collection_id) s/Uuid
+   ;(s/optional-key :media_entry_id) s/Uuid
+   (s/optional-key :highlight) s/Bool
+   (s/optional-key :cover) s/Bool
+   (s/optional-key :order) s/Num
+   (s/optional-key :position) s/Int})
+
 (def ring-routes
   ["/collection-media-entry-arcs"
    ["/" {:get {:summary "Get collection media-entry arcs."
                :handler arcs
                :swagger {:produces "application/json"}
                :coercion reitit.coercion.schema/coercion
-               :responses {200 {:body s/Any}}}
-         ; TODO
+               :parameters {:query {(s/optional-key :collection_id) s/Uuid
+                                    (s/optional-key :media_entry_id) s/Uuid}}
+               :responses {200 {:body s/Any}} ; TODO response coercion
+               }
+
          :post {:summary (sd/sum_todo "Create collection media-entry arc")
-                :handler (constantly sd/no_impl)}
-         }] ; TODO response coercion
+                :handler handle_create-col-me-arc
+                :swagger {:produces "application/json"}
+                :coercion reitit.coercion.schema/coercion
+                :parameters {:path {:collection_id s/Uuid
+                                    :media_entry_id s/Uuid}
+                             :body schema_collection-media-entry-arc-create}
+                :responses {200 {:body s/Any}
+                            406 {:body s/Any}}
+                }
+         }]
 
    ["/:id" {:get {:summary "Get collection media-entry arcs."
                   :handler arc
                   :swagger {:produces "application/json"}
                   :coercion reitit.coercion.schema/coercion
                   :parameters {:path {:id s/Str}}
-                  :responses {200 {:body s/Any}}}}] ; TODO response coercion
+                  :responses {200 {:body s/Any}
+                              404 {:body s/Any}} ; TODO response coercion
+                  }
+            ; TODO
+            :put {:summary (sd/sum_todo "Update collection media-entry arc")
+                  :handler (constantly sd/no_impl)
+                  :swagger {:produces "application/json"}
+                  :coercion reitit.coercion.schema/coercion
+                  :parameters {:path {:id s/Str}
+                               :body schema_collection-media-entry-arc-update}
+                  :responses {200 {:body s/Any}
+                              404 {:body s/Any}
+                              406 {:body s/Any}}
+                  }
+            ; TODO
+            :delete {:summary (sd/sum_todo "Delete collection media-entry arc")
+                     :handler (constantly sd/no_impl)
+                     :swagger {:produces "application/json"}
+                     :coercion reitit.coercion.schema/coercion
+                     :parameters {:path {:id s/Str}}
+                     :responses {200 {:body s/Any}
+                                 404 {:body s/Any}
+                                 406 {:body s/Any}}
+                     }
+            }] 
+   
    ])
 
 ;### Debug ####################################################################
