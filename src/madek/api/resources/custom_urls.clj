c87c5ed7f4b216d8642dcc8bfaddaaa3808e494b
Alex Liebrich <alexanderliebrich@web.de>
2023-03-21 22:54:31 +0100

    custom_urls post

--- src/madek/api/resources/custom_urls.clj
+++ src/madek/api/resources/custom_urls.clj
@@ -1,9 +1,12 @@
 (ns madek.api.resources.custom-urls 
-  (:require [clojure.tools.logging :as logging]
-            [madek.api.resources.shared :as sd]
-            [reitit.coercion.schema]
-            [schema.core :as s]
-            [madek.api.resources.media-resources.permissions :as mr-permissions]))
+  (:require
+   [clojure.java.jdbc :as jdbc]
+   [madek.api.utils.rdbms :as rdbms :refer [get-ds]]
+   [clojure.tools.logging :as logging]
+   [madek.api.resources.shared :as sd]
+   [reitit.coercion.schema]
+   [schema.core :as s]
+   [madek.api.resources.media-resources.permissions :as mr-permissions]))
 
 ; TODO query be collection or media_entry
 (defn handle_list-custom-urls
@@ -31,24 +34,58 @@
         ]
     
     (logging/info "handle_get-custom-urls"
-                  ;"\nmr\n" mr
                   "\ntype\n" mr-type
                   "\nid\n" mr-id
                   "\ncol-name\n" col-name)
-    (if-let [result (sd/query-eq-find-one "custom_urls" col-name mr-id)] ;col-name mr-id)
+    (if-let [result (sd/query-eq-find-one "custom_urls" col-name mr-id)]
       (sd/response_ok result)
       (sd/response_not_found (str "No such custom_url for " mr-type " with id: " mr-id)))
     ))
 
 (defn handle_create-custom-urls
   [req]
-  (sd/response_ok {:message "ok"})
-  )
+  (let [mr (-> req :media-resource)
+        mr-type (-> mr :type)
+        mr-id (-> mr :id str)
+        col-name (if (= mr-type "MediaEntry") "media_entry_id" "collection_id")
+        data (-> req :parameters :body)
+        dwid (if (= mr-type "MediaEntry")
+               (assoc data :media_entry_id mr-id)
+               (assoc data :collection_id mr-id))
+        ]
+    
+    (logging/info "handle_create-custom-urls"
+                  "\ntype\n" mr-type
+                  "\nid\n" mr-id
+                  "\ncol-name\n" col-name
+                  "\ndwid\n" dwid)
+    (if-let [ins-res (first (jdbc/insert! (rdbms/get-ds) :custom_urls dwid))]
+      (sd/response_ok ins-res) 
+      (sd/response_failed "Could not create custom_url." 406)
+    )
+  ))
+
+(def schema_create_custom_url
+  {:id s/Str
+   :is_primary s/Bool
+   :creator_id s/Uuid
+   :updator_id s/Uuid})
+
+(def schema_export_custom_url
+  {:id s/Str
+   :is_primary s/Bool
+   :creator_id s/Uuid
+   :updator_id s/Uuid
+   :updated_at s/Any
+   :created_at s/Any
+   :media_entry_id (s/maybe s/Uuid)
+   :collection_id (s/maybe s/Uuid)
+   })
 
 (def ring-routes
   ["/custom_urls"
    ["/"
-    {:get {:summary (sd/sum_todo "List custom_urls.")
+    {:get {:summary (sd/sum_usr "List custom_urls.")
            :handler handle_list-custom-urls
            :coercion reitit.coercion.schema/coercion
            :parameters {:query {(s/optional-key :full-data) s/Bool
@@ -56,7 +93,7 @@
                                 (s/optional-key :collection_id) s/Uuid}}}
     }]
    ["/:id"
-    {:get {:summary (sd/sum_todo "Get custom_url.")
+    {:get {:summary (sd/sum_usr "Get custom_url.")
            :handler handle_get-custom-url
            :coercion reitit.coercion.schema/coercion
            :parameters {:path {:id s/Str}}
@@ -73,14 +110,19 @@
                        sd/ring-wrap-authorization]
           :coercion reitit.coercion.schema/coercion
           :parameters {:path {:media_entry_id s/Str}}
+          :responses {200 {:body schema_export_custom_url}
+                      404 {:body s/Any}}
           }
     
-    :post {:summary (sd/sum_todo "Create custom_url for media entry.")
-           :handler (constantly sd/no_impl)
+    :post {:summary (sd/sum_usr "Create custom_url for media entry.")
+           :handler handle_create-custom-urls
            :middleware [sd/ring-wrap-add-media-resource
                         sd/ring-wrap-authorization]
            :coercion reitit.coercion.schema/coercion
-           :parameters {:path {:media_entry_id s/Str}}}
+           :parameters {:path {:media_entry_id s/Str}
+                        :body schema_create_custom_url}
+           :responses {200 {:body schema_export_custom_url}
+                       406 {:body s/Any}}}
     
     :delete {:summary (sd/sum_todo "Delete custom_url for media entry.")
              :handler (constantly sd/no_impl)}
@@ -96,12 +138,15 @@
           :coercion reitit.coercion.schema/coercion
           :parameters {:path {:collection_id s/Str}}}
 
-    :post {:summary (sd/sum_todo "Create custom_url for collection.")
-           :handler (constantly sd/no_impl)
+    :post {:summary (sd/sum_usr "Create custom_url for collection.")
+           :handler handle_create-custom-urls
            :middleware [sd/ring-wrap-add-media-resource
                         sd/ring-wrap-authorization]
            :coercion reitit.coercion.schema/coercion
-           :parameters {:path {:collection_id s/Str}}}
+           :parameters {:path {:collection_id s/Str}
+                        :body schema_create_custom_url}
+           :responses {200 {:body schema_export_custom_url}
+                       406 {:body s/Any}}}
     
     :delete {:summary (sd/sum_todo "Delete custom_url for collection.")
              :handler (constantly sd/no_impl)}}])
