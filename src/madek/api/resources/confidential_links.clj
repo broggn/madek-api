a742a55b285e7436e91ff3f56d6ef6c428509d59
mradl <manuel.radl@zhdk.ch>
2024-04-09 12:32:41 +0200

    links & contexts

--- src/madek/api/resources/confidential_links.clj
+++ src/madek/api/resources/confidential_links.clj
@@ -152,8 +152,8 @@
 
 ; TODO check can edit permissions
 (def user-me-routes
-
   ["/media-entry/:media_entry_id"
+   {:swagger {:tags ["api/media-entry"] :security [{"auth" []}]}}
    ["/conf-links"
     {:post {:summary (sd/sum_adm "Create confidential link.")
             :handler handle_create-conf-link
@@ -211,6 +211,7 @@
 ; TODO check can edit permissions
 (def user-col-routes
   ["/collection/:collection_id"
+   {:swagger {:tags ["api/collection/conf-links"] :security [{"auth" []}] }}
    ["/conf-links"
     {:post {:summary (sd/sum_adm "Create confidential link.")
             :handler handle_create-conf-link
